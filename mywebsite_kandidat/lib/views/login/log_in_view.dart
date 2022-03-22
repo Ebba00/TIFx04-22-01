f4305448514c0bb1395a905141d7bb4f3788c5ea
Hannesgustavsson <Hannesgustavsson65@gmail.com>
2022-03-22 23:51:03 +0100

    Update log_in_view.dart
    Skapade UI för inloggningssidan, inte klart än

--- mywebsite_kandidat/lib/views/login/log_in_view.dart
+++ mywebsite_kandidat/lib/views/login/log_in_view.dart
@@ -8,18 +8,90 @@ class LogInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: 400,
-          width: 400,
-          child: const NavBarItem('Logga in', PlayersRoute),
+    return Column(
+      children: [
+        const SizedBox(height: 50,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[50],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 300,
+                  width: 350,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const <Widget> [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text('LOGGA IN',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 50,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey[50],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              alignment: Alignment.center,
+              child: const Text('ELLER',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[50],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 300,
+                  width: 350,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const <Widget> [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text('SKAPA ETT KONTO',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        ),
-
-      );
-
-
-
+      ],
+    );
   }
 }
+
