214996dbc83c87ed1b6fbf9f6f70781f9dec641e
Ebba00 <ebba.molinder@me.com>
2022-03-28 21:04:06 +0200

    Matrix

--- mywebsite_kandidat/lib/views/Players/heatmap.dart
+++ mywebsite_kandidat/lib/views/Players/heatmap.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter/flutter_heat_map.dart';
 
-var row = 90;
-var col = 50;
-final List<List<int>> matrix2 = new List.generate(row, (i) =>
-    List.generate(col, (j) => 0));
+
+
+List<List<int>> randomMatrix(List<List<int>> matrix){
+
+  for (var i = 0; i < 90; i++){
+    for (var j = 0; j < 90; j++){
+      if (i == 45 && j == 25){
+        matrix[i][j] = 7;
+      }
+      else if (i % 10 == 0 && j % 10 == 0){
+        matrix[i][j] = 2;
+      }
+    }
+  }
+  return matrix;
+}
+
+
 
 // Heatmap
 // Nothing
--- mywebsite_kandidat/lib/views/Players/stats_view.dart
+++ mywebsite_kandidat/lib/views/Players/stats_view.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import '../../routing/route_names.dart';
 import '../../widgets/navigation_bar/navbar_logo.dart';
+import 'heatmap.dart';
+
+var row = 90;
+var col = 50;
+final List<List<int>> matrix2 = new List.generate(row, (i) =>
+    List.generate(col, (j) => 0));
+final List<List<int>> matrix3 = randomMatrix(matrix2);
+
 
 class StatsView extends StatelessWidget {
   const StatsView({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +24,9 @@ class StatsView extends StatelessWidget {
             height: 300,
             width: 580,
             decoration: BoxDecoration(color: Colors.grey[100],),
-            child: Image.asset('FloorballField.png'),
-          ),
-        ],
+            child: Image.asset('FloorballField.png')),
+            Center(child: Text(matrix3.toString())),
+          ],
       );
   }
 }
