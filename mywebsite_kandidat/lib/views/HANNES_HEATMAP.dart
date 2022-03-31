47b1bca46e8342c6b5e2ff40e670e822827eb7ce
Hannesgustavsson <Hannesgustavsson65@gmail.com>
2022-03-31 13:25:52 +0200

    sssss
    min heatmap

--- mywebsite_kandidat/lib/views/HANNES_HEATMAP.dart
+++ mywebsite_kandidat/lib/views/HANNES_HEATMAP.dart
@@ -1,16 +1,41 @@
+
 import 'package:flutter/material.dart';
 
+var coordinatesX = [12,24,5,7,3,29,15];
+var coordinatesY = [15,14,24,28,4,8,25];
+
 class HannesHeatmap extends StatelessWidget {
-  const HannesHeatmap({Key? key}) : super(key: key);
+  HannesHeatmap({Key? key}) : super(key: key);
+
+
+  var lengthCoordinates = coordinatesX.length;
+
+  var section11 = 0;
+  var section11Percent = 0.0;
+  var i = 0;
+
 
 
   @override
   Widget build(BuildContext context) {
+    while (i < lengthCoordinates) {
+      if (coordinatesX[i] < 20 && coordinatesY[i] < 20) {
+        section11++;
+        i++;
+      }
+      else {
+        i++;
+      }
+    }
+
+    section11Percent = (section11/lengthCoordinates);
+
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+
         Container(
           height: 210,
           width: 420,
@@ -23,6 +48,7 @@ class HannesHeatmap extends StatelessWidget {
                       height: 70,
                       width: 140,
                       color: Colors.orange,
+                      child: Text('$section11, $section11Percent'),
                       ),
                     Container(
                       height: 70,
@@ -32,7 +58,9 @@ class HannesHeatmap extends StatelessWidget {
                     Container(
                       height: 70,
                       width: 140,
-                      color: Colors.red,
+                      color: (section11Percent < 1)
+                        ? Colors.greenAccent
+                          : Colors.red
                     ),
                   ],
                 ),
