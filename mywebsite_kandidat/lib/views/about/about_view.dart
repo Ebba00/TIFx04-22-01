d66207dada2ccd233e329e6097d62a5dea6d7054
Hannesgustavsson <Hannesgustavsson65@gmail.com>
2022-05-11 15:00:00 +0200

    w
    w

--- mywebsite_kandidat/lib/views/about/about_view.dart
+++ mywebsite_kandidat/lib/views/about/about_view.dart
@@ -12,7 +12,7 @@ class AboutView extends StatelessWidget {
       children: [
         const SizedBox(
           height: 100,
-          width: 80,
+          width: 800,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -21,22 +21,23 @@ class AboutView extends StatelessWidget {
             SizedBox(
               width: 400,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                   children: const <Widget> [
-                    Text('VAD ÄR INDICATE?',
+                    Text('BAKGRUNDSINFORMATION',
                         style: TextStyle(
-                        fontWeight: FontWeight.w800, height: 1.15, fontSize: 30),
-                      textAlign: TextAlign.center,
+                        fontWeight: FontWeight.w800, height: 1.15, fontSize: 28),
+                      textAlign: TextAlign.left,
                     ),
                     SizedBox(height: 30,),
 
-                    Text('Välkommen till innebandytränarens'
-                        ' bästa vän och ett effektivt verktyg för att analysera spelarprestationer.'
-                        ' Skapa en profil och lägg till spelare eller läs mer om INDICATE och vad vi erbjuder.',
+                    Text('Denna hemsida utgör en del av'
+                        ' ett kandidatarbete som bedrivits under våren 2022 på Chalmers tekniska högskola.'
+                        ' Arbetet går vid institutionen för fysik och ämnar att skapa ett lättillgängligt'
+                        ' positioneringssystem i syfte att utvinna relevant spelarinformation.',
                       style: TextStyle(
                           fontSize: 15, height: 1.7),
-                      textAlign: TextAlign.center,
+                      textAlign: TextAlign.left,
                     ),
                   ],
               ),
@@ -44,22 +45,29 @@ class AboutView extends StatelessWidget {
             SizedBox(
               width: 400,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: const <Widget> [
-                  Text('VAD ÄR INDICATE?',
+                  Text('TEAMET BAKOM INDICATE',
                     style: TextStyle(
-                        fontWeight: FontWeight.w800, height: 1.15, fontSize: 30),
-                    textAlign: TextAlign.center,
+                        fontWeight: FontWeight.w800, height: 1.15, fontSize: 28),
+                    textAlign: TextAlign.left,
                   ),
                   SizedBox(height: 30,),
 
-                  Text('Välkommen till innebandytränarens'
-                      ' bästa vän och ett effektivt verktyg för att analysera spelarprestationer.'
-                      ' Skapa en profil och lägg till spelare eller läs mer om INDICATE och vad vi erbjuder.',
+                  Text('Följande är namnen på studenterna som utfört detta '
+                      'kandidatarbete samt programmen de tillhör:',
                     style: TextStyle(
                         fontSize: 15, height: 1.7),
-                    textAlign: TextAlign.center,
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 22,),
+
+                  Text('Hannes Gustafsson (M), Pontus Johansson (F), Oskar Kullner (I),'
+                  ' Ebba Molinder (D), Victor Salomonsson (TM).',
+                    style: TextStyle(
+                        fontSize: 15, height: 1.7),
+                    textAlign: TextAlign.left,
                   ),
                 ],
               ),
