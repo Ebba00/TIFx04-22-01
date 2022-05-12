d9105caec17770823e6f2496dff00fb8b948d8f6
Ebba00 <ebba.molinder@me.com>
2022-05-12 14:18:27 +0200

    value of king

--- mywebsite_kandidat/lib/views/Players/all_players/Database.dart
+++ mywebsite_kandidat/lib/views/Players/all_players/Database.dart
@@ -47,8 +47,8 @@ List<List<double>> match0XCoord = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x1
 List<List<double>> match0YCoord = [y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15,y16,y17,y18,y19,y20];
 
 // match1
-List<double> x1a = [];
-List<double> y1a = [];
+List<double> x1a = [1,2,4,4,4,4,7];
+List<double> y1a = [5,6,7,3,0,9,2];
 List<double> x2a = [];
 List<double> y2a = [];
 List<double> x3a = [];
